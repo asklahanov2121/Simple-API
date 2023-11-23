71c7dc0e50435f80bd377cee0b81704e7840b9e2
asklahanov2121 <128796933+asklahanov2121@users.noreply.github.com>
2023-11-23 17:08:04 +0800

    Create api.php

---
+++ api.php
@@ -0,0 +1,30 @@
+<?php
+define('API_KEYS', array(
+    'kNLbNKNSJyhAFDXEcjw2Aj9Q98D7s2td',
+    'sPLvQQWtYbCArE8DaGReagrEvavYXRnc',
+    '7DQwkCUcJwp7D5F5xb4aAPwpqD44hPdS'
+));
+function returnUnauthorized($message) {
+    header("HTTP/1.1 401 Unauthorized");
+    print('{"exception":"' . $message . '"}');
+    exit();
+}
+$headers = apache_request_headers();
+if(isset($headers['Authorization'])) {
+    $authorizationHeader = explode(" ", $headers['Authorization']);
+    if(isset($authorizationHeader[0]) &&  $authorizationHeader[0] === 'Apikey') {
+        if(isset($authorizationHeader[1]) &&  in_array($authorizationHeader[1], API_KEYS)) {
+
+            echo "HAHA NAKUHA MO!";
+        }
+        else {
+            returnUnauthorized('Apikey supplied does not match any authorized keys.');
+        }
+    }
+    else {
+        returnUnauthorized('Unsupported Authorization type.');
+    }
+}
+else {
+    returnUnauthorized('Missing Authorization header.');
+}
